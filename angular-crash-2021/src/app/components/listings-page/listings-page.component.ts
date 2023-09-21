d92b6c937d14e113a0d87d85e19c7de26fabf4f7
Kalindu Sembakutti <sembukkk@mcmaster.ca>
2023-09-21 10:30:10 -0400

    Listings OnInit Fix

--- angular-crash-2021/src/app/components/listings-page/listings-page.component.ts
+++ angular-crash-2021/src/app/components/listings-page/listings-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TaskService } from '../../services/task.service';
 import { Task } from '../../Task';
 
