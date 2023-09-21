4f602dc031f6d33a9dd64874a1fe2e8413ea1c00
Kalindu Sembakutti <sembukkk@mcmaster.ca>
2023-09-21 10:22:57 -0400

    Listings Page Created

--- angular-crash-2021/src/app/components/listings-page/listings-page.component.ts
+++ angular-crash-2021/src/app/components/listings-page/listings-page.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../Task';
 
 @Component({
   selector: 'app-listings-page',
   templateUrl: './listings-page.component.html',
   styleUrls: ['./listings-page.component.css']
 })
-export class ListingsPageComponent {
+export class ListingsPageComponent implements OnInit {
+  tasks: Task[] = [];
 
+  constructor(private taskService: TaskService) {
+    this.day = formatDate(this.date, 'mmmm dS', 'en-US', '+0530');
+  }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
+  }
+
+  addTask(task: Task) {
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
+  }
 }
