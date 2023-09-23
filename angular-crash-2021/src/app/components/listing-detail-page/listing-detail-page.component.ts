6d246f3ecd09be6cf95ec8b8846e9df7fe3e73f6
Kalindu Sembakutti <35012510+kalkutti@users.noreply.github.com>
2023-09-23 15:42:53 -0400

    Update listing-detail-page.component.ts

--- angular-crash-2021/src/app/components/listing-detail-page/listing-detail-page.component.ts
+++ angular-crash-2021/src/app/components/listing-detail-page/listing-detail-page.component.ts
@@ -18,6 +18,6 @@ export class ListingDetailPageComponent {
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
-    this.taskService.getTasks().subscribe((tasks) => (this.task = tasks.filter((t) => t.id == id)));
+    this.taskService.getTasks().subscribe((tasks) => (this.task = tasks.find((t) => t.id == id)));
   }
 }
